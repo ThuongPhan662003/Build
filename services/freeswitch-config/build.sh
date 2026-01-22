22d3f2fe3f872d17d31792c263bcc4525cc5d9df
ThuongPhan662003 <thuong5so55555@gmail.com>
2026-01-22 15:14:00 +0700

    fix freeswitch config

--- services/freeswitch-config/build.sh
+++ services/freeswitch-config/build.sh
@@ -1,4 +1,5 @@
 # đảm bảo quyền executable cho maintainer scripts
+chmod 0755 services/freeswitch-conf/DEBIAN/control   2>/dev/null || true
 chmod 0755 services/freeswitch-conf/DEBIAN/postinst 2>/dev/null || true
 chmod 0755 services/freeswitch-conf/DEBIAN/preinst  2>/dev/null || true
 chmod 0755 services/freeswitch-conf/DEBIAN/prerm    2>/dev/null || true
