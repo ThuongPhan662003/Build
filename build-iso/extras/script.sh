f6d6f12d2c0733e73f557a7ba3bd3428e1deb104
ThuongPhan662003 <thuong5so55555@gmail.com>
2026-01-22 23:42:31 +0700

    push build iso

---
+++ build-iso/extras/script.sh
@@ -0,0 +1,9 @@
+build-simple-cdd --conf simple-cdd.conf --dvd \
+  --local-packages /home/thuong/build/extras/nginxrepo \
+  --local-packages /home/thuong/build/extras/nginx-config \
+  --local-packages /home/thuong/build/extras/phpsuryrepo\
+  --local-packages /home/thuong/build/extras/fusionpbx\
+  --local-packages /home/thuong/build/extras/php-config\
+  --local-packages /home/thuong/build/extras/pgrepo\
+  --local-packages /home/thuong/build/extras/postgresql-config\
+  --local-packages /home/thuong/build/extras/freeswitch
