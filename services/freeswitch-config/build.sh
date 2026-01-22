1540745fb312df86339a83e4d893bb0bf85b9c7b
Tô Phan Kiều Thương <125787847+ThuongPhan662003@users.noreply.github.com>
2026-01-22 15:31:33 +0700

    Update build.sh

--- services/freeswitch-config/build.sh
+++ services/freeswitch-config/build.sh
@@ -5,4 +5,4 @@ chmod 0755 services/freeswitch-config/DEBIAN/preinst  2>/dev/null || true
 chmod 0755 services/freeswitch-config/DEBIAN/prerm    2>/dev/null || true
 chmod 0755 services/freeswitch-config/DEBIAN/postrm   2>/dev/null || true
 #
-dpkg-deb --build services/freeswitch-conf
+dpkg-deb --build services/freeswitch-config
