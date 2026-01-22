783c21de1359b2448537bc8538731921fd81c6ee
Tô Phan Kiều Thương <125787847+ThuongPhan662003@users.noreply.github.com>
2026-01-22 15:28:43 +0700

    Update build.sh

--- services/freeswitch-config/build.sh
+++ services/freeswitch-config/build.sh
@@ -1,8 +1,8 @@
 # đảm bảo quyền executable cho maintainer scripts
-chmod 0755 services/freeswitch-conf/DEBIAN/control   2>/dev/null || true
-chmod 0755 services/freeswitch-conf/DEBIAN/postinst 2>/dev/null || true
-chmod 0755 services/freeswitch-conf/DEBIAN/preinst  2>/dev/null || true
-chmod 0755 services/freeswitch-conf/DEBIAN/prerm    2>/dev/null || true
-chmod 0755 services/freeswitch-conf/DEBIAN/postrm   2>/dev/null || true
+chmod 0755 services/freeswitch-config/DEBIAN/control   2>/dev/null || true
+chmod 0755 services/freeswitch-config/DEBIAN/postinst 2>/dev/null || true
+chmod 0755 services/freeswitch-config/DEBIAN/preinst  2>/dev/null || true
+chmod 0755 services/freeswitch-config/DEBIAN/prerm    2>/dev/null || true
+chmod 0755 services/freeswitch-config/DEBIAN/postrm   2>/dev/null || true
 #
 dpkg-deb --build services/freeswitch-conf
